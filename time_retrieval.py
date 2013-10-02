bcfc760f7f475d99c1168d132969b7222d37cd07
Thomas Shields <me@thomasshields.net>
2013-10-02 19:47:06 -0400

    added some fields, formatting, etc.

--- time_retrieval.py
+++ time_retrieval.py
@@ -20,7 +20,7 @@ for course in courses:
 	urlsoc.close()
 
 	# Splits code into pieces that should each have one section, except the first which is discarded
-	section_code_list = sourcedata.split("<th CLASS=\"ddheader\" scope=\"col\" >Type</th>")[1:]
+	section_code_list = sourcedata.split("<th CLASS=\"ddtitle\"")[1:]
 
 	sections = []
 	for section_code in section_code_list:
@@ -28,8 +28,23 @@ for course in courses:
 		class_regex = "<td CLASS=\"dddefault\">Class</td>\n<td CLASS=\"dddefault\">(.*?) - (.*?)</td>\n<td CLASS=\"dddefault\">(.*?)</td>\n<td CLASS=\"dddefault\">(.*?)</td>"
 		raw_meetings = re.findall(class_regex, section_code)
 		if raw_meetings:
-			meetings = [ { "start_time": raw_meeting[0], "end_time": raw_meeting[1], "days": list(raw_meeting[2]), "location": raw_meeting[3] } for raw_meeting in raw_meetings ]
-			section = {"college": course["college"], "id": course["id"], "meetings": meetings}
+			credits = "NA"
+			raw_credits= re.search("(\d\.\d{3}) Credits",section_code)
+			if raw_credits:
+				credits = raw_credits.group(1)
+			meetings = [ {
+							"start_time": raw_meeting[0],
+							"end_time": raw_meeting[1],
+							"days": list(raw_meeting[2]),
+							"location": raw_meeting[3]
+						} for raw_meeting in raw_meetings ]
+			section = {
+						"college":	course["college"],
+						"title":	course["title"],
+						"id":		course["id"],
+						"credits":	credits,
+						"meetings":	meetings
+					}
 			sections.append(section)
 
 	# Currently treating sections as independent and not grouping by course
@@ -44,7 +59,7 @@ for course in courses:
 # 	print ""
 
 # Putting sections in json
-sections_json = json.dumps(all_sections)
+sections_json = json.dumps(all_sections, indent=4, sort_keys=True)
 f = open("sections.json", "w")
 f.write(sections_json)
 f.close()
