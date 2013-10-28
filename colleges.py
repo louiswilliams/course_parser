f3d301b0b9a86c8be94c20be47ace0ba195a2433
extrakt <louieaw@gmail.com>
2013-10-28 16:39:03 -0400

    Updated courses as of 10/28/13
    Also changed the word "college" in all references to the word "school"
    for clarification.

--- colleges.py
+++ colleges.py
@@ -1,11 +1,19 @@
 import re,json
 
-raw_html = open("oscar_courseform.htm", "r").read()
+raw_html = open("coursesearch.html", "r").read()
 
 #html_regex = "<OPTION VALUE=\"(.*)\">(.*)"
 html_regex = "<option value=\"(.*)\">(.*)"
 
-colleges = re.findall(html_regex,raw_html)
+schools_raw = re.findall(html_regex,raw_html)
 
-for college in colleges:
-	print "'" + college[0] +"' => '" + college[1] + "',"
+schools = []
+
+for school_raw in schools_raw:
+	school = {}
+	school["name"] = school_raw[0]
+	school["title"] = school_raw[1]
+	schools.append(school)
+	print school
+
+open("schools.json", "w").write(json.dumps(schools))
--- parser.py
+++ parser.py
@@ -1,23 +1,22 @@
 import re,json,urllib2
 
-# This creates a list of colleges to use when POSTing the Oscar page
-colleges_html = open("oscar_courseform.htm", "r").read()
-colleges_regex = "<option value=\"(.*)\">(.*)"
-colleges = re.findall(colleges_regex,colleges_html)
+# This creates a list of schools to use when POSTing the Oscar page
+schools_json = open("schools.json", "r").read()
+schools = json.loads(schools_json)
 
 courses = []
 
 # Oscar URL to post request
 url = "https://oscar.gatech.edu/pls/bprod/bwckctlg.p_display_courses"
-for college in colleges:
+for school in schools:
 	# Some hacking to figure this out
-	data = "term_in=201308&call_proc_in=bwckctlg.p_disp_dyn_ctlg&sel_subj=dummy&sel_levl=dummy&sel_schd=dummy&sel_coll=dummy&sel_divs=dummy&sel_dept=dummy&sel_attr=dummy&sel_subj=" + college[0] + "&sel_crse_strt=&sel_crse_end=&sel_title="
+	data = "term_in=201402&call_proc_in=bwckctlg.p_disp_dyn_ctlg&sel_subj=dummy&sel_levl=dummy&sel_schd=dummy&sel_coll=dummy&sel_divs=dummy&sel_dept=dummy&sel_attr=dummy&sel_subj=" + school["name"] + "&sel_crse_strt=&sel_crse_end=&sel_title="
 	urlsoc = urllib2.urlopen(url,data)
 	raw_html = urlsoc.read()
 	urlsoc.close()
 
-	# College name
-	print college[1]
+	# school name
+	print school["title"]
 
 	courses_html = raw_html.split("<td CLASS=\"nttitle\"")[1:]
 	course_regex = "<a href=\".*\">([A-Z]*) ([\w]*) - (.*)</a>"
@@ -29,20 +28,19 @@ for college in colleges:
 		raw_course = re.findall(course_regex,course_html)[0]	
 		course = {}
 
-		course["college"] = raw_course[0]
+		course["school"] = raw_course[0]
 		course["id"] =  raw_course[1] 
 		course["title"] = raw_course[2]
 		credits = re.findall(credit_regex,course_html)
 		# Some courses are weird and have no "Credit hours". 
 		# This just sets credits to 0 if there are none 
-		if len(credits) < 1:
+		if len(credits) == 1:
 			course["credit"] = credits[0]
 		else:
 			course["credit"] = 0.000
 		courses.append(course)
 
 		print course
-
 courses_json_str = json.dumps(courses)
 # print courses
 open("courses.json", "w").write(courses_json_str)
